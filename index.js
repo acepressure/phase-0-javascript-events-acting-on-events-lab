a6203f87597fa1c0a1921c2aad2b0a991cee15b1
Walter Simon <simonwalter986@gmail.com>
2022-06-23 04:19:28 +0300

    commit tree 42eb748c4e401abba500c3e067272341080b3888

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        console.log('howdy')
+        moveDodgerLeft();
+    }
+  });
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+        console.log('howdy')
+        moveDodgerRight();
+    }
+  });
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left > 5) {
+      dodger.style.left = `${left - 1}px`;
+      console.log(left)
+    }
+  }
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left < 180) {
+      dodger.style.left = `${left + 1}px`;
+      console.log(left)
+    }
+  } 
